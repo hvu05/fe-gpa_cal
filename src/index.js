1cbfbf312d587ab37dc1b778de3da7631a8f076d
hvu05 <hoangvu06052005@gmail.com>
2025-09-15 00:16:47 +0700

    fix modal Antd

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import 'antd/dist/reset.css'; // 👈 ở đây
+import { ConfigProvider } from 'antd'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
-        <App />
-
-    </BrowserRouter>
+    <ConfigProvider>
+      <App />
+    </ConfigProvider>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
