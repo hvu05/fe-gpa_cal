e65190166cfe33d9cad3c8582d19982424ba44d2
hvu05 <hoangvu06052005@gmail.com>
2025-09-15 10:25:49 +0700

    change with ant in index.js outside

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import './App.css';
 
 import './style/main.scss'
 import Allrouter from './components/Allroutes';
-import { ConfigProvider } from 'antd';
-import 'antd/dist/reset.css';   // AntD v5
 function App() {
   return (
     <ConfigProvider>
--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { ConfigProvider } from 'antd';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
-        <App />
-
-    </BrowserRouter>
+    <ConfigProvider>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </ConfigProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
