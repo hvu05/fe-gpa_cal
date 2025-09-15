1130908b1f102b1662245b6bbfb4ac2142b07615
hvu05 <hoangvu06052005@gmail.com>
2025-09-15 10:34:43 +0700

    change with ant in index.js outside

--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import { ConfigProvider } from 'antd';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
-        <ConfigProvider>
             <App />
-        </ConfigProvider>
     </BrowserRouter>
 
 );
